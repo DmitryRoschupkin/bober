0d1201e8aa59fd848afb0ec5710f648809692167
DmitryRoschupkin <dmitrii.roshupkin@gmail.com>
2026-08-01 21:42:01 +0500

    initian commit: frontend (parallax on the main page)

---
+++ src/main/java/me/dmitriy/bober/BoberApplication.java
@@ -0,0 +1,13 @@
+package me.dmitriy.bober;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoberApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/dmitriy/bober/BoberApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dmitriy.bober;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

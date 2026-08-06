7222f840aa980481404335391d5660d249ebda22
DmitryRoschupkin <dmitrii.roshupkin@gmail.com>
2026-08-06 10:35:55 +0500

    base for authorization and admin's management

---
+++ src/main/java/me/dmitriy/bober/web/security/AuthorizationController.java
@@ -0,0 +1,20 @@
+package me.dmitriy.bober.web.security;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorizationController {
+
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "login-page";
+    }
+
+    @GetMapping("/registration")
+    public String getRegistrationPage() {
+        return "registration-page";
+    }
+}

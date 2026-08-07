4da737b9cb10674705a5508187cef26a2bb495df
DmitryRoschupkin <dmitrii.roshupkin@gmail.com>
2026-08-07 14:53:41 +0500

    auth pages + profile: make frontend great again

--- src/main/java/me/dmitriy/bober/web/AccountController.java
+++ src/main/java/me/dmitriy/bober/web/AccountController.java
@@ -4,11 +4,13 @@ package me.dmitriy.bober.web;
 import me.dmitriy.bober.data.UserRepository;
 import me.dmitriy.bober.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
 
@@ -24,7 +26,9 @@ public class AccountController {
 
     @GetMapping("/{id}")
     public String getAccount(Model model, @PathVariable int id) {
-        Optional<User> user = userRepository.findById(id);
+        User user = userRepository
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User "+id+" not found"));
         model.addAttribute("user", user);
         return "account";
     }
